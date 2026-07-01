6fb0432d73eaacc3dcd656e917d557718376d852
Bravinsimiyu <bravinwasike18@gmail.com>
2026-07-01 19:02:20 +0300

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path"
 import cookieParser from "cookie-parser";
+import path from "path";
 
-import authRoutes from "./routes/auth.route.js"
-import productRoutes from "./routes/product.route.js"
-import cartRoutes from "./routes/cart.route.js"
-import couponRoutes from "./routes/coupon.route.js"
-import paymentRoutes from "./routes/payment.route.js"
-import analyticsRoutes from "./routes/analytics.route.js"
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
-
 
 dotenv.config();
 
@@ -20,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json({limit: "10mb"})) //allows you to parse the body of the the request. (req.body)
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -30,16 +29,15 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  })
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-
 app.listen(PORT, () => {
-  console.log("Server started at http://localhost:" + PORT);
-  connectDB();
-})
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
+});
